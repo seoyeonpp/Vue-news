9529a44ee47f47ec0f775e04daf5c57da9040524
박서연 <seoyeonpub@gmail.com>
2023-01-21 21:57:12 +0900

    커밋

---
+++ vue-news/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app">
+        <ToolBar />
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+import ToolBar from "@/components/ToolBar.vue";
+export default {
+    components: {
+        ToolBar,
+    },
+};
+</script>
+
+<style></style>
