58d02b69a5a58dc2018477eafe95274001f3ff33
박서연 <seoyeonpub@gmail.com>
2023-01-23 18:51:06 +0900

    화면구성 수정

--- vue-news/src/views/JobsView.vue
+++ vue-news/src/views/JobsView.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
-        <div v-for="job in this.$store.state.jobs" :key="job">{{ job.title }}</div>
+        <p v-for="item in this.$store.state.jobs" :key="item">
+            <a :href="item.url">{{ item.title }}</a>
+            <small>{{ item.time_ago }}, {{ item.domain }}</small>
+        </p>
     </div>
 </template>
 
